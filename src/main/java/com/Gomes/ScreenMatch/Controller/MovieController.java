d61ab23e382725e2ad7a8ebdac938ecd578d65ed
Arthur Gomes <72204367+arthuzim@users.noreply.github.com>
2023-05-31 10:02:26 -0300

    was maked interaction between register movie and movie list

--- src/main/java/com/Gomes/ScreenMatch/Controller/MovieController.java
+++ src/main/java/com/Gomes/ScreenMatch/Controller/MovieController.java
@@ -3,6 +3,7 @@ package com.Gomes.ScreenMatch.Controller;
 import com.Gomes.ScreenMatch.Domain.Movie.dadosRegisterMovie;
 import com.Gomes.ScreenMatch.Models.Movies;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,15 @@ import java.util.List;
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
+
     private List<Movies> movie = new ArrayList<>();
     @GetMapping("/form")
     public  String loadFormPage(){
         return "Movies/form";
     }
     @GetMapping
-    public  String listMovies(){
+    public  String listMovies(Model model){
+        model.addAttribute("list",movie);
         return "Movies/listMovies";
     }
 
